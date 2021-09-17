1fa638ca6024a39d3d5f340b9a5cab24b208182d
alin-kk <65072397+alin-kk@users.noreply.github.com>
2021-09-17 20:13:24 +0800

    Update NowFileStructure.java

--- src/main/java/com/gradle/demo/NowFileStructure.java
+++ src/main/java/com/gradle/demo/NowFileStructure.java
@@ -26,7 +26,6 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.ui.content.ContentManager;
 import com.intellij.util.ui.TimerUtil;
-import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -36,26 +35,32 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.List;
 
 public class NowFileStructure {
     DefaultMutableTreeNode root = new DefaultMutableTreeNode("Interest Code Elements");
     ArrayList allPaths = new ArrayList<String>();
     ArrayList newpath = new ArrayList<String>();
+
     private Project project;
     private ToolWindow toolWindow;
     private ToolWindow showToolWindow;
     private VirtualFile myFile;
     private StructureView structureView;
+
     private boolean firstRefresh = true;
     private int count;
     private int nowId;
+
     public NowFileStructure(Project project,ToolWindow toolWindow) {
         ToolWindowManager.getInstance(project).registerToolWindow(new RegisterToolWindowTask("InterestCodeElement", ToolWindowAnchor.RIGHT,null,true,false,true,true,null, AllIcons.Toolwindows.ToolWindowStructure,null));
         ToolWindowManager windowManager = ToolWindowManager.getInstance(project);
         showToolWindow = windowManager.getToolWindow("InterestCodeElement");
+
         showToolWindow.show();
         this.project = project;
         this.toolWindow = toolWindow;
+
         Timer timer = TimerUtil.createNamedTimer("nowFileStructure", 100, event -> {
             int count = ActivityTracker.getInstance().getCount();
             if (count == this.count) {
@@ -69,17 +74,19 @@ public class NowFileStructure {
     private void refresh() {
         Component owner = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
         boolean ifContact = SwingUtilities.isDescendingFrom(toolWindow.getComponent(), owner);
+
         DataContext dataContext = DataManager.getInstance().getDataContext(owner);
         if (CommonDataKeys.PROJECT.getData(dataContext) != project || (!firstRefresh && (ifContact || JBPopupFactory.getInstance().isPopupActive()))){
             return;
         }
+
         VirtualFile[] files = ifContact ? null : CommonDataKeys.VIRTUAL_FILE_ARRAY.getData(dataContext);
         if (files != null && files.length == 1) {
             newFile(files[0]);
         }
         else if (firstRefresh) {
             FileEditorManagerImpl editorManager = (FileEditorManagerImpl) FileEditorManager.getInstance(project);
-            java.util.List<Pair<VirtualFile, EditorWindow>> history = editorManager.getSelectionHistory();
+            List<Pair<VirtualFile, EditorWindow>> history = editorManager.getSelectionHistory();
             if (!history.isEmpty()) {
                 newFile(history.get(0).getFirst());
             }
@@ -89,13 +96,14 @@ public class NowFileStructure {
     private void newFile(VirtualFile file) {
         if (!Comparing.equal(file, myFile)) {
             myFile = file;
-            rebuild();
+            build();
         }
     }
 
-    public void rebuild() {
+    public void build() {
         ContentManager contentManager = toolWindow.getContentManager();
         contentManager.removeAllContents(true);
+
         VirtualFile file = myFile;
         if (file == null) {
             VirtualFile[] selectedFiles = FileEditorManager.getInstance(project).getSelectedFiles();
@@ -104,7 +112,6 @@ public class NowFileStructure {
             }
         }
 
-        String[] names = {""};
         FileEditor editor = FileEditorManager.getInstance(project).getSelectedEditor(file);
         if (file.isDirectory() || editor == null || !editor.isValid()) {
             return;
@@ -139,19 +146,18 @@ public class NowFileStructure {
         });
     }
 
-private void createPanel(Component component){
-    MyPanel myPanel = new MyPanel();
-    myPanel.setBackground(UIUtil.getTreeBackground());
-    myPanel.add(component, BorderLayout.CENTER);
-    Content content = ContentFactory.SERVICE.getInstance().createContent(myPanel, "", false);
-    ContentManager contentManager = toolWindow.getContentManager();
-    contentManager.removeAllContents(true);
-    contentManager.addContent(content);
-}
-    private class MyPanel extends JPanel{
-        MyPanel() {
-            super(new BorderLayout());
-        }
+    private void createPanel(Component component){
+        JPanel myPanel = new JPanel();
+        myPanel.setLayout(new BorderLayout());
+        myPanel.add(component, BorderLayout.CENTER);
+        createContent(myPanel);
+    }
+
+    private void createContent(JPanel myPanel) {
+        Content content = ContentFactory.SERVICE.getInstance().createContent(myPanel, "", false);
+        ContentManager contentManager = toolWindow.getContentManager();
+        contentManager.removeAllContents(true);
+        contentManager.addContent(content);
     }
 
     private void buildTree(){
@@ -161,13 +167,19 @@ private void createPanel(Component component){
             insertNode.add(newNode);
             insertNode = newNode;
         };
+
         ContentManager myContentManager = showToolWindow.getContentManager();
         myContentManager.removeAllContents(true);
+        createShowContent();
+    }
+
+    private void createShowContent() {
         InterestCodeElementsImpl myInterest = new InterestCodeElementsImpl(root,allPaths);
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(myInterest.getContent(), "", false);
         showToolWindow.getContentManager().addContent(content);
     }
+
     private DefaultMutableTreeNode selectInsertNode(){
         DefaultMutableTreeNode nowNode = root;
         DefaultMutableTreeNode insertNode = root;
